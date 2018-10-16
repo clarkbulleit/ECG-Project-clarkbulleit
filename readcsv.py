058e3ddd4e1c6a0477bb0043dd1ca356edd81456
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-16 19:51:50 -0400

    put time and voltage lists inside of dictionaries

--- readcsv.py
+++ readcsv.py
@@ -1,23 +1,21 @@
 
-def readcsv(file):
+def readcsv(file, num):
     import csv
     with open(file) as csv_file:
         readCSV = csv.reader(csv_file, delimiter=',')
 
-        time = []
-        voltage = []
+        rawdata = {}
+        rawdata["time"] = []
+        rawdata["voltage"] = []
 
         for row in readCSV:
             t = float(row[0])
             v = float(row[1])
-            time.append(t)
-            voltage.append(v)
+            rawdata["time"].append(t)
+            rawdata["voltage"].append(v)
 
-    return time, voltage
+    return rawdata
 
 
 if __name__ == "__main__":
-    try:
-        print(readcsv('test_data/test_data1.csv'))
-    except TypeError:
-        print("file contains non integer values")
+        readcsv('test_data/test_data1.csv', 1)
