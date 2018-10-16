deb490995d3123aaf4ca89d7fdeeb956df74ad69
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-16 13:57:31 -0400

    fixes bugs to open csv file in differet folder than script, imports data into lists

--- readcsv.py
+++ readcsv.py
@@ -1,23 +1,12 @@
 import csv
 
 
-def readcsv(file):
-
-    """
-    :param file: insert name of file
-    :return: strings with info from files
-    """
-
-
-with open('test_data1.csv') as csv_file:
+with open('test_data/test_data1.csv') as csv_file:
     readCSV = csv.reader(csv_file, delimiter=',')
 
     times = []
     voltages = []
 
     for row in readCSV:
-        time = row[0]
-        voltage = row[1]
-
-        times = times.append(time)
-        voltages = voltages.append(voltage)
+        times.append(row[0])
+        voltages.append(row[1])
