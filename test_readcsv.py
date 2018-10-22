2557a2aaf5fb0aa6c4018e52e0466532648464ba
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-22 09:10:08 -0400

    tests length of the string to make sure that the function is skipping over string lines

--- test_readcsv.py
+++ test_readcsv.py
@@ -3,10 +3,14 @@
 def test_readcsv():
     from readcsv import readcsv
     raw_data = readcsv('test_readcsv.csv')
-    assert raw_data["time"][0] == 1
+    t = raw_data["time"]
+    v = raw_data["voltage"]
 
-    for x in raw_data["time"]:
+    assert t[0] == 1
+    assert len(t) == 2
+
+    for x in t:
         assert type(x) == float
 
-    for y in raw_data["voltage"]:
+    for y in v:
         assert type(y) == float
