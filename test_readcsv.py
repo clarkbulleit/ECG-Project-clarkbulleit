f5ffb2b2968716e8d894f3bcf7e1d25999c46b9d
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-22 09:05:13 -0400

    adds assertion for data type of voltage

--- test_readcsv.py
+++ test_readcsv.py
@@ -4,4 +4,9 @@ def test_readcsv():
     from readcsv import readcsv
     raw_data = readcsv('test_readcsv.csv')
     assert raw_data["time"][0] == 1
-    assert type(raw_data["time"][0]) == float
+
+    for x in raw_data["time"]:
+        assert type(x) == float
+
+    for y in raw_data["voltage"]:
+        assert type(y) == float
