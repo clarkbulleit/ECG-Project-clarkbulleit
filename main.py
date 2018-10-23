3cec61b5dc7e85c267301a4b598d581170338893
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-23 16:17:13 -0400

    deleted main file code for easy merging

--- main.py
+++
@@ -1,7 +0,0 @@
-from readcsv import readcsv
-
-if __name__ == "__main__":
-            #raw_data = readcsv('test_data/test_data5.csv')
-            #print(raw_data)
-            #validate_data(raw_data)
-            #peak_detect(raw_data, .3, .01)
