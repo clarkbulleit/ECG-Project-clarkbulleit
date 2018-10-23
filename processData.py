9da366aaa74c75bf1ec579b30119b7f76748a179
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-23 13:52:52 -0400

    created peak_detect function using peakutils

--- processData.py
+++ processData.py
@@ -7,3 +7,23 @@ def plot_data(data):
 
     plt.plot(t, v)
     plt.show()
+
+
+def peak_detect(data, thres=1.5, min_dist=100):
+    import peakutils
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    v = data["voltage"]
+    t = data["time"]
+    cb = np.array(v)
+    inds = peakutils.indexes(cb, thres, min_dist)
+    locs_peaks = []
+    height_peaks = []
+
+    for x in inds:
+        locs_peaks.append(t(x))
+        height_peaks.append(v(x))
+
+    plt.plot(t, v, locs_peaks, height_peaks, 'ro')
+    plt.show()
