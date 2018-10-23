d50916d43478bf4a3ab732360ec8318f9df0830c
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-23 14:34:39 -0400

    removed print raw_data from main

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ from processData import peak_detect
 
 if __name__ == "__main__":
             raw_data = readcsv('test_data/test_data5.csv')
-            print(raw_data)
             validate_data(raw_data)
             peak_detect(raw_data, .3, .01)
