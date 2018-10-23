3447dedbcfb23e09f1de23d16f5888eb55eb0555
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-23 16:14:59 -0400

    spliced functions into their own files

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from readcsv import readcsv
-from readcsv import validate_data
-from processData import peak_detect
-
 
 if __name__ == "__main__":
-            raw_data = readcsv('test_data/test_data5.csv')
-            print(raw_data)
-            validate_data(raw_data)
-            peak_detect(raw_data, .3, .01)
+            #raw_data = readcsv('test_data/test_data5.csv')
+            #print(raw_data)
+            #validate_data(raw_data)
+            #peak_detect(raw_data, .3, .01)
--- readcsv.py
+++ readcsv.py
@@ -25,26 +25,3 @@ def readcsv(file):
                 continue
 
     return raw_data
-
-
-def validate_data(data):
-    """
-
-    :param data: receives input of a dictionary with lists under the keys for
-    "time" and "voltage"
-    :return:
-    """
-    t = data["time"]
-    v = data["voltage"]
-    exceeds_range = []
-
-    if len(t) != len(v):
-        print("the lengths are not equal")
-
-    for x in t:
-        if x < 0:
-            print("negative time values present")
-
-    for y in v:
-        if y > 300:
-            exceeds_range.append(y)
