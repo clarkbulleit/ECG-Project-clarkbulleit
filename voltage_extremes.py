e38c77e0667f67051ad5d88ba4f710345f693c91
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 12:48:41 -0400

    added doctrings for voltage_extremes

--- voltage_extremes.py
+++ voltage_extremes.py
@@ -1,5 +1,11 @@
 
 def voltage_extremes(data):
+    """
+    :param data: dictionary with list under key
+    "voltage"
+    :return: tuple containing minimum of list in 0 index
+    and maximum of list in 1 index
+    """
     voltage = data["voltage"]
 
     v_extremes = (min(voltage), max(voltage))
