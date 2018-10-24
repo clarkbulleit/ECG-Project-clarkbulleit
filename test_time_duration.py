bb2ebd37674c5a673963f2cc14f714e36b20b861
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 13:14:35 -0400

    made sure that function output float values

--- test_time_duration.py
+++ test_time_duration.py
@@ -7,9 +7,9 @@ def test_time_duration():
     """
     from time_duration import time_duration
 
-    data = {'time': [0, 1.2, 1.7, 1.9, 2], 'voltage': [1, 2,
+    data = {'time': [0.0, 1.2, 1.7, 1.9, 2.7], 'voltage': [1, 2,
                                                        3, 4, 5]}
 
     duration = time_duration(data)
 
-    assert duration == 2
+    assert duration == 2.7
