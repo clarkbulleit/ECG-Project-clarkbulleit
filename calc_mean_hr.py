22597c07f7d701c5e452420e7d883d7b58c14688
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 13:22:20 -0400

    added docstrings

--- calc_mean_hr.py
+++ calc_mean_hr.py
@@ -1,5 +1,10 @@
 
 def calc_mean_hr(num_beats, duration):
+    """
+    :param num_beats: int
+    :param duration: float
+    :return: mean hr in beats per minute as int
+    """
 
     mean_hr_bpm = round((num_beats/duration)*60)
 
