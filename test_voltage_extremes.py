fe26c23adce1f273fb8d7458a1f6cde5aa435ada
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 12:47:03 -0400

    created test function for voltage_extremes function

--- test_voltage_extremes.py
+++ test_voltage_extremes.py
@@ -1,3 +1,9 @@
 
 def test_voltage_extremes():
     from voltage_extremes import voltage_extremes
+
+    data = {'time': [1, 2, 3], 'voltage': [1, 2, 3]}
+
+    v_extremes = voltage_extremes(data)
+
+    assert v_extremes == (1, 3)
