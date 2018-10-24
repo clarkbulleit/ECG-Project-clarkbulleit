732cc81d1ac016a8039f87aea506a8b6c220dc9e
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 11:51:36 -0400

    added docstrings

--- test_validate_data.py
+++ test_validate_data.py
@@ -1,5 +1,13 @@
 
 def test_validate_data():
+    """
+    Unit test for validate_data function
+    :return: proper errors for two bad inputs
+    Makes sure validate_data throws DiffListLengthError
+    if the two lists are not the same length
+    Makes sure validate_data throws NegTimeValueError
+    if the time list contains negative time values
+    """
     from validate_data import validate_data, \
         DiffListLengthError, NegTimeValueError
     import pytest
