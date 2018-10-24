466757e6057dcfe0635bf0c98ccd3accaaad383f
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 13:22:39 -0400

    created unit test for calc_mean_hr function

--- test_calc_mean_hr.py
+++ test_calc_mean_hr.py
@@ -1,3 +1,10 @@
 
 def test_calc_mean_hr():
     from calc_mean_hr import calc_mean_hr
+
+    num_beats = 50
+    duration = 30.2
+
+    mean_hr_bpm = calc_mean_hr(num_beats, duration)
+
+    assert mean_hr_bpm == 99
