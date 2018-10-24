9631ee16a2d4133bedbef1af20a46b4668e24cca
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 12:37:55 -0400

    added docstrings

--- test_time_duration.py
+++ test_time_duration.py
@@ -1,5 +1,10 @@
 
 def test_time_duration():
+    """
+    tests time_duration function
+    :return: assertion that the function returns
+    the proper values
+    """
     from time_duration import time_duration
 
     data = {'time': [0, 1.2, 1.7, 1.9, 2], 'voltage': [1, 2,
--- time_duration.py
+++ time_duration.py
@@ -1,5 +1,11 @@
 
 def time_duration(data):
+    """
+
+    :param data: dictionary with list under
+    key "time"
+    :return: duration of the time list
+    """
     t = data["time"]
     duration = t[-1] - t[0]
 
