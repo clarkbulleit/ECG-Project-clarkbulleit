b0a719c3408ef59ccd2b19ec3036a5aef723fb1e
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-23 22:10:07 -0400

    created two different bad inputs to trigger errors seperately

--- test_validate_data.py
+++ test_validate_data.py
@@ -5,10 +5,11 @@ def test_validate_data():
     from validate_data import NegTimeValueError
     import pytest
 
-    bad_input = {'time': [1, -1, 3], 'voltage': [1, 2]}
+    bad_input1 = {'time': [1, 1, 3], 'voltage': [1, 2]}
+    bad_input2 = {'time': [-1, 1], 'voltage': [1, 2]}
 
     with pytest.raises(DiffListLengthError):
-        validate_data(bad_input)
+        validate_data(bad_input1)
 
     with pytest.raises(NegTimeValueError):
-        validate_data(bad_input)
+        validate_data(bad_input2)
