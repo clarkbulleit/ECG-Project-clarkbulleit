39e191c5119120585b76b53e59d89a15d7760c84
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 13:03:13 -0400

    created unit test for count_beats

--- test_count_beats.py
+++ test_count_beats.py
@@ -1,3 +1,10 @@
 
 def test_cound_beats():
     from count_beats import count_beats
+    import numpy as np
+
+    list = np.array([1, 2, 3, 4])
+    beats = count_beats(list)
+
+    assert beats == 4
+
