fae92a638d88f853f67717044e17866d5e27d937
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 13:44:25 -0400

    pep8 changes

--- test_create_dict.py
+++ test_create_dict.py
@@ -11,5 +11,5 @@ def test_create_dict():
 
     metrics = create_dict(mean_hr_bpm, voltage_extremes, duration, num_beats, beats)
 
-    assert metrics == {'mean_hr_bpm': 60, 'voltage_extremes': (0, 10), 'duration': 33.5, 'num_beats': 40,
-                       'beats': np.array([1, 2, 3, 4, 5])}
+    assert np.all(metrics == {'mean_hr_bpm': 60, 'voltage_extremes': (0, 10), 'duration': 33.5,
+                              'num_beats': 40, 'beats': np.array([1, 2, 3, 4, 5])})
