834c1b02af39538adb7e7619a61592d084dd768d
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-23 22:06:59 -0400

    imported pytest and test that function calls custom errors

--- test_validate_data.py
+++ test_validate_data.py
@@ -1,3 +1,14 @@
 
 def test_validate_data():
     from validate_data import validate_data
+    from validate_data import DiffListLengthError
+    from validate_data import NegTimeValueError
+    import pytest
+
+    bad_input = {'time': [1, -1, 3], 'voltage': [1, 2]}
+
+    with pytest.raises(DiffListLengthError):
+        validate_data(bad_input)
+
+    with pytest.raises(NegTimeValueError):
+        validate_data(bad_input)
