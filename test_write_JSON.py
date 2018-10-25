fa63c8b45d20689971e3e878caf6324c084612c7
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 15:51:32 -0400

    writes test_write_JSON function

--- test_write_JSON.py
+++ test_write_JSON.py
@@ -2,3 +2,13 @@
 
 def test_write_JSON():
     from write_JSON import write_JSON
+    import json
+    metrics = {'beats': [1, 2, 3, 4], 'duration': 1, 'num_beats': [1, 2]}
+
+    write_JSON(metrics, 'test_write_Json')
+
+    with open('test_write_Json.json') as read_file:
+        m = json.load(read_file)
+
+    assert metrics == m
+
