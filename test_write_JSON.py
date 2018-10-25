f04b753ec1eab718b28ba43920c39c0de6fad097
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 16:03:11 -0400

    adds docstrings (closes #44)

--- test_write_JSON.py
+++ test_write_JSON.py
@@ -1,6 +1,12 @@
 
 
 def test_write_JSON():
+    """ Unit test for write_JSON file
+
+    Writes a dictionary to a JSON file
+    Then reads that file and asserts that
+    it is equal to the original dictionary
+    """
     from write_JSON import write_JSON
     import json
     metrics = {'beats': [1, 2, 3, 4], 'duration': 1, 'num_beats': [1, 2]}
--- write_JSON.py
+++ write_JSON.py
@@ -1,5 +1,13 @@
 
 def write_JSON(metrics, filename):
+    """
+
+    :param metrics: dictionary with appropriate metrics
+    :param filename: filename that JSON is being
+    written to
+    :return: JSON file with dictionary entered in
+    alphabetic order
+    """
     import json
     with open(filename + '.json', "w") as write_file:
         json.dump(metrics, write_file, sort_keys=True, indent=4)
