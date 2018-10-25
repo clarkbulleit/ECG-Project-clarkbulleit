8794ba53fbc4bcf8b744e18a1cf84e057c331cc6
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 14:55:30 -0400

    moves pytest.raises assertion into its own function

--- test_calc_mean_hr.py
+++ test_calc_mean_hr.py
@@ -10,10 +10,17 @@ def test_calc_mean_hr(a, b, expected):
     :return: Asserts that the function outputs
     the correct hr as an integer
     """
-    from calc_mean_hr import calc_mean_hr, NoBeatsDetected
-    import pytest
+    from calc_mean_hr import calc_mean_hr
 
     assert calc_mean_hr(a, b) == expected
 
+
+def test_calc_mean_hr_raises():
+    import pytest
+    from calc_mean_hr import calc_mean_hr, NoBeatsDetected
+
+    bad_num = 0
+    bad_dur = 10
+
     with pytest.raises(NoBeatsDetected):
-        calc_mean_hr()
+        calc_mean_hr(bad_num, bad_dur)
