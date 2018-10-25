d0b7ad5a1b4d797c4ef5dba37725d77636b36fcf
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 14:45:25 -0400

    docstring additions

--- calc_mean_hr.py
+++ calc_mean_hr.py
@@ -1,9 +1,9 @@
 
 def calc_mean_hr(num_beats, duration):
     """
-    :param num_beats: int
-    :param duration: float
-    :return: mean hr in beats per minute as int
+    :param num_beats: Int of number of detected beats
+    :param duration: Float of duration of ECG strip
+    :return: Mean hr in beats per minute as int
     """
 
     mean_hr_bpm = round((num_beats/duration)*60)
