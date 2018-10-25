6608c934e76b98ec6925752227816b3cdfb2be6a
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 16:53:34 -0400

    converts numpy arrays to lists

--- test_write_JSON.py
+++ test_write_JSON.py
@@ -9,7 +9,8 @@ def test_write_JSON():
     """
     from write_JSON import write_JSON
     import json
-    metrics = {'beats': [1, 2, 3, 4], 'duration': 1, 'num_beats': [1, 2]}
+    import numpy as np
+    metrics = {'beats': np.array([1, 2, 3, 4]), 'duration': 1, 'num_beats': [1, 2]}
 
     write_JSON(metrics, 'test_write_Json')
 
