fb18d0564b6952e826d48f6234e57ed9323b671a
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 14:22:29 -0400

    adds more parameters for testing

--- test_voltage_extremes.py
+++ test_voltage_extremes.py
@@ -4,6 +4,8 @@ import pytest
 @pytest.mark.parametrize("a,expected", [
     ({'voltage': [1, 2, 3]}, (1, 3)),
     ({'voltage': [1, 2, 3, 3]}, (1, 3)),
+    ({'voltage': [-1, 2, 3]}, (-1, 3)),
+    ({'voltage': [-1, -3]}, (-3, -1)),
 ])
 def test_voltage_extremes(a, expected):
     """
