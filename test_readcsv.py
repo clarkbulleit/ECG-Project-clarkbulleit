a577b5d63f94f10f887a2c0816fe52cf54fe4f85
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 21:42:04 -0400

    added negative assertion

--- test_readcsv.py
+++ test_readcsv.py
@@ -7,4 +7,6 @@ def test_readcsv():
     from readcsv import readcsv
     raw_data = readcsv('test_readcsv.csv')
 
-    assert raw_data == {'time': [1, 7], 'voltage': [2, 9]}
+    assert raw_data == {'time': [1, 7, 1.2], 'voltage': [2, 9, 4.5]}
+    assert raw_data != {'time': [1, '', 7, 'c', 1.2], 'voltage': [2, 6,
+                                                                  9, 10, 4.5]}
