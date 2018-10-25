3ab00570b7e8e82461045173af6c475f651db998
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 15:30:33 -0400

    added docstring info

--- create_dict.py
+++ create_dict.py
@@ -2,6 +2,16 @@
 
 def create_dict(mean_hr_bpm, voltage_extremes, duration, num_beats,
                 beats):
+    """ Creates dictionary from 5 inputs with specified keys
+
+    :param mean_hr_bpm: integer for mean heart rate
+    :param voltage_extremes: tuple for min and max voltages
+    :param duration: duration of ECG time strip
+    :param num_beats: integer of number of beats
+    :param beats: numpy list of time locations of peaks
+    :return: the complied dictionary of all the terms
+    under the correct keys
+    """
     metrics = {}
     metrics["mean_hr_bpm"] = mean_hr_bpm
     metrics["voltage_extremes"] = voltage_extremes
