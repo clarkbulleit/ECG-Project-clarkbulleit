0de13fb60a1779aca8f19a6d878690e842c0bfbb
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 20:55:01 -0400

    created main function to see output

--- write_JSON.py
+++ write_JSON.py
@@ -2,4 +2,9 @@
 def write_JSON(metrics, filename):
     import json
     with open(filename + '.json', "w") as write_file:
-        json.dump(metrics, write_file)
+        json.dump(metrics, write_file, sort_keys=True, indent=4)
+
+
+if __name__ == "__main__":
+    metrics = {'num_beats': (1, 2), 'beats': [1, 2, 3, 4], 'duration': 1}
+    write_JSON(metrics, 'firstJSON')
