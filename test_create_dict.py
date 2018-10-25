916ce128dae58de1d0ad10cad7ab7b517cfd0eef
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 15:28:12 -0400

    tested each value associated with each key

--- test_create_dict.py
+++ test_create_dict.py
@@ -13,7 +13,7 @@ def test_create_dict():
                           num_beats, beats)
 
     assert metrics["mean_hr_bpm"] == 60
-
-    # np.all(metrics == {'mean_hr_bpm': 60, 'voltage_extremes': (0, 10),
-    #  'duration': 33.5,
-    # 'num_beats': 40, 'beats': np.array([1, 2, 3])})
+    assert metrics["voltage_extremes"] == (0, 10)
+    assert metrics["duration"] == 33.5
+    assert metrics["num_beats"] == 40
+    assert np.all(metrics["beats"] == np.array([1, 2, 3]))
