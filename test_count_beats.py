32079634622dbded124d2282577dcea6253a9a61
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 22:17:41 -0400

    fixed name

--- test_count_beats.py
+++ test_count_beats.py
@@ -6,7 +6,7 @@ import pytest
     (np.array([1, 2, 3, 4]), 4),
     (np.array([1, 2]), 2),
 ])
-def test_cound_beats(a, expected):
+def test_count_beats(a, expected):
     """
     Unit test for count_beats function
     :return: assertion that test outputs
