3b18056df82e0a30dd166c4d9c05c5a6ed66c332
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 19:32:30 -0400

    removed unneccesary lines of code

--- readcsv.py
+++ readcsv.py
@@ -30,7 +30,6 @@ def readcsv(file):
                 nn = nn + 1
 
                 if nn != 0:
-                    # print("Warning: " + str(nn) + " Rows with non-numeric inputs were not imported")
                     logging.warning(str(nn) + "Rows with non-numeric inputs were not imported")
                 continue
 
