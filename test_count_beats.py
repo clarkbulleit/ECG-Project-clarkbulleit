6cfce5334054e2de3cab3e937dfacfe1934ad4fa
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-24 22:16:39 -0400

    implemented parametrized testing

--- test_count_beats.py
+++ test_count_beats.py
@@ -1,15 +1,19 @@
+import numpy as np
+import pytest
 
-def test_cound_beats():
+
+@pytest.mark.parametrize("a,expected", [
+    (np.array([1, 2, 3, 4]), 4),
+    (np.array([1, 2]), 2),
+])
+
+
+def test_cound_beats(a, expected):
     """
     Unit test for count_beats function
     :return: assertion that test outputs
     correct number of elements in array
     """
     from count_beats import count_beats
-    import numpy as np
 
-    list = np.array([1, 2, 3, 4])
-    beats = count_beats(list)
-
-    assert beats == 4
-    assert beats != 3
+    assert count_beats(a) == expected
