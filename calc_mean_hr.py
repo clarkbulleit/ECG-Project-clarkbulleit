c7ec89ef2e63615a1d1aa39d02f20d54ae1c8cbe
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 14:46:56 -0400

    added custom error to find when no peaks were detected

--- calc_mean_hr.py
+++ calc_mean_hr.py
@@ -1,10 +1,23 @@
 
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class NoBeatsDetected(Error):
+    """Raised when the x and y lists are different leng"""
+    pass
+
+
 def calc_mean_hr(num_beats, duration):
     """
     :param num_beats: Int of number of detected beats
     :param duration: Float of duration of ECG strip
     :return: Mean hr in beats per minute as int
     """
+
+    if num_beats == 0:
+        raise NoBeatsDetected
 
     mean_hr_bpm = round((num_beats/duration)*60)
 
