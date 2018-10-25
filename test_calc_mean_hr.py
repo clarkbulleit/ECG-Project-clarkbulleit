5ad9067e5ec678de66ebb869ebf34d5828a06f94
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 14:59:16 -0400

    adds additional tests for type error

--- test_calc_mean_hr.py
+++ test_calc_mean_hr.py
@@ -3,6 +3,7 @@ import pytest
 
 @pytest.mark.parametrize("a,b,expected", [
     (40, 30, 80),
+    (40.25, 97.8, 25),
 ])
 def test_calc_mean_hr(a, b, expected):
     """
@@ -19,8 +20,13 @@ def test_calc_mean_hr_raises():
     import pytest
     from calc_mean_hr import calc_mean_hr, NoBeatsDetected
 
-    bad_num = 0
+    bad_num1 = 0
     bad_dur = 10
 
+    bad_num2 = 'c'
+
     with pytest.raises(NoBeatsDetected):
-        calc_mean_hr(bad_num, bad_dur)
+        calc_mean_hr(bad_num1, bad_dur)
+
+    with pytest.raises(TypeError):
+        calc_mean_hr(bad_num2, bad_dur)
