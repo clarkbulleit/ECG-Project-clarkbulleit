931fa2f0d75ad3d817425aa13ace9ef483134191
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 14:50:50 -0400

    implements test for raising the error when no beats were detected

--- test_calc_mean_hr.py
+++ test_calc_mean_hr.py
@@ -5,11 +5,14 @@ def test_calc_mean_hr():
     :return: Asserts that the function outputs
     the correct hr as an integer
     """
-    from calc_mean_hr import calc_mean_hr
+    from calc_mean_hr import calc_mean_hr, NoBeatsDetected
+    import pytest
 
     num_beats = 50
     duration = 30.2
+    bad_num = 0
 
-    mean_hr_bpm = calc_mean_hr(num_beats, duration)
+    assert calc_mean_hr(num_beats, duration) == 99
 
-    assert mean_hr_bpm == 99
+    with pytest.raises(NoBeatsDetected):
+        calc_mean_hr(bad_num, duration)
