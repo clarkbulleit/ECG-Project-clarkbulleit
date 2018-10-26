63efbbac60c67d0df40619fe226eccddf60079aa
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-26 18:03:18 -0400

    google style docstring formatting for function voltage_extremes

--- voltage_extremes.py
+++ voltage_extremes.py
@@ -1,10 +1,15 @@
 
 def voltage_extremes(data):
-    """
-    :param data: Dictionary with list under key
-    "voltage"
-    :return: Tuple containing minimum of list in 0 index
-    and maximum of list in 1 index
+    """ Finds min and max of voltage ECG trace
+
+    Args:
+        :param data: Dictionary with list under key
+        "voltage"
+
+    Return:
+        v_extremes: Tuple containing minimum of list in 0 index
+        and maximum of list in 1 index
+
     """
     voltage = data["voltage"]
 
