67835c529975635b7a80b413679b60d78200d915
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-25 23:18:28 -0400

    adds log inputs if the heart rate is too high or too low

--- calc_mean_hr.py
+++ calc_mean_hr.py
@@ -18,6 +18,10 @@ def calc_mean_hr(num_beats, duration, beats=[1, 2, 3], user_average=1):
     Raises an error if the num_beats input = 0
     Raises TypeError if non-numeric
     """
+    import logging
+    logging.basicConfig(filename="Main_Log.txt",
+                        format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
     new_num_beats = 0
 
     if num_beats == 0:
@@ -31,5 +35,11 @@ def calc_mean_hr(num_beats, duration, beats=[1, 2, 3], user_average=1):
             if x < user_average*60:
                 new_num_beats = new_num_beats + 1
         mean_hr_bpm = round((new_num_beats/user_average))
+
+    if mean_hr_bpm < 30:
+        logging.warning("Warning: The heart rate is below normal range")
+
+    elif mean_hr_bpm > 200:
+        logging.warning("Warning: The hear rate exceeds normal values")
 
     return mean_hr_bpm
