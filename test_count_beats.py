566ebdb70b6b48d061c847bf515305a102552063
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-26 17:37:04 -0400

    removed docstring for test_count_beats

--- test_count_beats.py
+++ test_count_beats.py
@@ -7,11 +7,6 @@ import pytest
     (np.array([1, 2]), 2),
 ])
 def test_count_beats(a, expected):
-    """
-    Unit test for count_beats function
-    :return: Assertion that test outputs
-    correct number of elements in array
-    """
     from count_beats import count_beats
 
     assert count_beats(a) == expected
