abcaed1cb9dc7fdbf538a80d6236ded66796dca6
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-26 17:39:43 -0400

    removed docstring for test_voltage_extremes

--- test_voltage_extremes.py
+++ test_voltage_extremes.py
@@ -8,14 +8,6 @@ import pytest
     ({'voltage': [-1, -3]}, (-3, -1)),
 ])
 def test_voltage_extremes(a, expected):
-    """
-    Unit test for voltage_extremes function
-    creates input in form of dictionary
-    :return: Asserts that a tuple was created
-    of the form (min(list), max(list))
-    Tests multiple max values, single negatives,
-    and doule negatives
-    """
     from voltage_extremes import voltage_extremes
 
     assert voltage_extremes(a) == expected
