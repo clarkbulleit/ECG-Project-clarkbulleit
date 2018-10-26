4c47e4deaea3def311a026a16d8d5ad7545a6598
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-26 17:38:58 -0400

    removed docstring for test_time_duration

--- test_time_duration.py
+++ test_time_duration.py
@@ -1,10 +1,5 @@
 
 def test_time_duration():
-    """
-    Tests time_duration function
-    :return: Assertion that the function returns
-    the proper value for duration
-    """
     from time_duration import time_duration
 
     data = {'time': [0.0, 1.2, 1.7, 1.9, 2.7], 'voltage': [1, 2, 3, 4, 5]}
