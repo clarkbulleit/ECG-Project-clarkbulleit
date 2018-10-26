69b4312c53dcaf2a4b602bd02140d3a44d85068f
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-26 17:40:04 -0400

    removed docstring for test_write_JSON

--- test_write_JSON.py
+++ test_write_JSON.py
@@ -1,14 +1,6 @@
 
 
 def test_write_JSON():
-    """ Unit test for write_JSON file
-
-    Writes a dictionary to a JSON file
-    Then reads that file and asserts that
-    it is equal to the original dictionary
-    Especially tests that numpy arrays can
-    be written to file
-    """
     from write_JSON import write_JSON
     import json
     import numpy as np
